ebc2a99b4f57e60227fa9ade2f10174e9f839aeb
D2460N <D2460N@protonmail.com>
2020-03-09 15:20:51 +0100

    essai

--- DI.java
+++ DI.java
@@ -1,27 +1,14 @@
 package com.openclassrooms.entrevoisins.di;
 
-import com.openclassrooms.entrevoisins.model.Neighbour;
 import com.openclassrooms.entrevoisins.service.DummyNeighbourApiService;
 import com.openclassrooms.entrevoisins.service.NeighbourApiService;
-
-import java.util.List;
 
 /**
  * Dependency injector to get instance of services
  */
 public class DI {
 
-    private static NeighbourApiService service = new DummyNeighbourApiService() {
-        @Override
-        public Neighbour getSpecificNeighbour(Integer id) {
-            return null;
-        }
-
-        @Override
-        public List<Neighbour> getFavoriteNeighbours(List<Integer> ids) {
-            return null;
-        }
-    };
+    private static NeighbourApiService service = new DummyNeighbourApiService();
 
     /**
      * Get an instance on @{@link NeighbourApiService}
@@ -36,16 +23,6 @@ public class DI {
      * @return
      */
     public static NeighbourApiService getNewInstanceApiService() {
-        return new DummyNeighbourApiService() {
-            @Override
-            public Neighbour getSpecificNeighbour(Integer id) {
-                return null;
-            }
-
-            @Override
-            public List<Neighbour> getFavoriteNeighbours(List<Integer> ids) {
-                return null;
-            }
-        };
+        return new DummyNeighbourApiService();
     }
 }
